860acc68e0f63ef0323972db07e1d96636642a78
Md Abu Bakkar Siddiqe Sajib <contact2sajib@gmail.com>
2024-02-07 17:26:23 +0600

    list set comprehension

---
+++ Functional Programming/list_set_comprehension.py
@@ -0,0 +1,19 @@
+my_list = []
+
+for item in 'hello':
+    my_list.append(item)
+
+print(my_list)
+
+my_list1 = [item for item in 'Sajib']
+print(my_list1)
+
+my_list2 = [num**2 for num in range(1,11)]
+print(my_list2)
+
+# only even squares
+my_set = {num**2 for num in range(1,11) if num**2 % 2 == 0}
+print(my_set)
+# remember that set don't contain duplicate values
+
+# for list use [] and for set use {}
