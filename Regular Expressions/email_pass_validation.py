d53992426b832f72696aadabc2a063abb62997fa
Md Abu Bakkar Siddiqe Sajib <contact2sajib@gmail.com>
2024-02-08 17:48:58 +0600

    regex

---
+++ Regular Expressions/email_pass_validation.py
@@ -0,0 +1,13 @@
+import re
+
+email_pattern = re.compile(r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+check_email = email_pattern.fullmatch('sajib@sajib.com')
+
+#Password check minimum 8 words also ends with number
+password_patter = re.compile(r"([A-Za-z0-9$%#@]{7,}[0-9])")
+check_password = password_patter.fullmatch('jibrish#%@8')
+
+if check_email and check_password:
+    print("Both email and password are correct.")
+else:
+    print("Try again.")
