50c8855cdffc0fe0e5a60f4150dba152138a6820
Md Abu Bakkar Siddiqe Sajib <contact2sajib@gmail.com>
2024-02-10 22:39:09 +0600

    merged pdf

---
+++ Scripting/PDF/merge_pdf.py
@@ -0,0 +1,12 @@
+import PyPDF2
+import sys
+
+inputs = sys.argv[1:]   # this will stores all the arguments passes except the first one, and store them in a list
+
+def pdf_merger(pdf_list):
+    merger = PyPDF2.PdfFileMerger()
+    for pdf in pdf_list:
+        merger.append(pdf)
+    merger.write('./merged.pdf')
+
+pdf_merger(inputs)
